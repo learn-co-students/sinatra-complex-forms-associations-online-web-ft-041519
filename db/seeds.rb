3b29551af6f4898ca1e84e81a344b0f0dae53aea
Ashleigh Chun <ashleigh.chun@gmail.com>
2019-06-08 18:08:28 -0700

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 # Add seed data here. Seed your database with `rake db:seed`
+#the code in the seed file will be executed, inserting some sample data into your database.
 sophie = Owner.create(name: "Sophie")
 Pet.create(name: "Maddy", owner: sophie)
 Pet.create(name: "Nona", owner: sophie)
